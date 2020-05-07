e7f91df663f8f682429991ea13e6d0ca237bcfa7
tgrzelak <tgrzelak@catalogicsoftware.com>
2020-05-07 11:32:22 +0200

    fix function and update tests

--- projects/ngc-charts/src/lib/utils/rotateAngle.spec.ts
+++ projects/ngc-charts/src/lib/utils/rotateAngle.spec.ts
@@ -1,29 +1,50 @@
 import { getRotateAngle } from './rotateAngle';
-describe('getRotateAngle', () => {
+fdescribe('[FUNCTION] getRotateAngle', () => {
   const rotateAngle = getRotateAngle;
 
   it('should return 0', () => {
-    expect(rotateAngle(20, 10)).toEqual(0);
+    const arrayLength = 20;
+    const arrayOfRandomNumbers = () => Array.from({length: arrayLength}, () => Math.floor(Math.random() * 100 ));
+
+    const columnsWidthPx: number[] = arrayOfRandomNumbers();
+    const labelsWidthPx: number[] = arrayOfRandomNumbers();
+
+    for (let i = 0; i < arrayLength; i++) {
+      if (columnsWidthPx[i] >= labelsWidthPx[i]) {
+        expect(rotateAngle(columnsWidthPx[i], labelsWidthPx[i])).toEqual(0);
+      }
+    }
   });
 
-  it('should return 0', () => {
-    expect(rotateAngle(50, 20)).toEqual(0);
+  it('should return -18', () => {
+    expect(rotateAngle(100, 105)).toEqual(-18);
   });
 
-  it('should return 0', () => {
-    expect(rotateAngle(75, 50)).toEqual(0);
+  it('should return -34', () => {
+    expect(rotateAngle(100, 120)).toEqual(-34);
   });
 
-  it('should return -45', () => {
-    expect(rotateAngle(10, 20)).toEqual(-45);
+  it('should return -48', () => {
+    expect(rotateAngle(100, 150)).toEqual(-48);
   });
 
-  it('should return -45', () => {
-    expect(rotateAngle(20, 50)).toEqual(-45);
+  it('should return -60', () => {
+    expect(rotateAngle(100, 200)).toEqual(-60);
   });
 
-  it('should return -45', () => {
-    expect(rotateAngle(50, 75)).toEqual(-45);
+  it('should return -71', () => {
+    expect(rotateAngle(100, 300)).toEqual(-71);
   });
 
+  it('should return -76', () => {
+    expect(rotateAngle(100, 400)).toEqual(-76);
+  });
+
+  it('should return -78', () => {
+    expect(rotateAngle(100, 500)).toEqual(-78);
+  });
+
+  it('should return -90', () => {
+    expect(rotateAngle(100, 12000)).toEqual(-90);
+  });
 });
--- projects/ngc-charts/src/lib/utils/rotateAngle.ts
+++ projects/ngc-charts/src/lib/utils/rotateAngle.ts
@@ -1,3 +1,9 @@
 export const getRotateAngle = (columnWidthPx: number, labelWidthPx: number): number => {
-  return labelWidthPx > columnWidthPx ? -45 : 0;
+  if (labelWidthPx > columnWidthPx) {
+    const acos = Math.acos(columnWidthPx / labelWidthPx);
+    const roundedResult = Math.round(acos * 180 / Math.PI);
+    return -Math.abs(roundedResult);
+  } else {
+    return 0;
+  }
 };
