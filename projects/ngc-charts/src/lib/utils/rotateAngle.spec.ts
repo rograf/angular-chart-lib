723e1893de98190c17ff9707def3962e5facb741
tgrzelak <tgrzelak@catalogicsoftware.com>
2020-05-14 10:24:06 +0200

    modifications in tests

--- projects/ngc-charts/src/lib/utils/rotateAngle.spec.ts
+++ projects/ngc-charts/src/lib/utils/rotateAngle.spec.ts
@@ -2,49 +2,67 @@ import { getRotateAngle } from './rotateAngle';
 fdescribe('[FUNCTION] getRotateAngle', () => {
   const rotateAngle = getRotateAngle;
 
-  it('should return 0', () => {
+  describe('', () => {
     const arrayLength = 20;
-    const arrayOfRandomNumbers = () => Array.from({length: arrayLength}, () => Math.floor(Math.random() * 100 ));
+    let arrayOfRandomNumbers;
+    let columnsWidthPx: number[];
+    let labelsWidthPx: number[];
 
-    const columnsWidthPx: number[] = arrayOfRandomNumbers();
-    const labelsWidthPx: number[] = arrayOfRandomNumbers();
+    beforeEach(() => {
+      arrayOfRandomNumbers = () => Array.from({length: arrayLength}, () => Math.floor(Math.random() * 100 ));
+      columnsWidthPx = arrayOfRandomNumbers();
+      labelsWidthPx = arrayOfRandomNumbers();
+    });
 
-    for (let i = 0; i < arrayLength; i++) {
-      if (columnsWidthPx[i] >= labelsWidthPx[i]) {
-        expect(rotateAngle(columnsWidthPx[i], labelsWidthPx[i])).toEqual(0);
+    it('should return 0', () => {
+      for (let i = 0; i < arrayLength; i++) {
+        if (columnsWidthPx[i] >= labelsWidthPx[i]) {
+          expect(rotateAngle(columnsWidthPx[i], labelsWidthPx[i])).toEqual(0);
+        }
       }
-    }
+    });
+
+    it('should return value from range [-90, 0]', () => {
+      for (let i = 0; i < arrayLength; i++) {
+        if (columnsWidthPx[i] < labelsWidthPx[i]) {
+          expect(rotateAngle(columnsWidthPx[i], labelsWidthPx[i])).toBeGreaterThan(-90);
+          expect(rotateAngle(columnsWidthPx[i], labelsWidthPx[i])).toBeLessThan(0);
+        }
+      }
+    });
   });
 
-  it('should return -18', () => {
-    expect(rotateAngle(100, 105)).toEqual(-18);
-  });
+  describe('examples', () => {
+    it('[100, 105] should return -18', () => {
+      expect(rotateAngle(100, 105)).toEqual(-18);
+    });
 
-  it('should return -34', () => {
-    expect(rotateAngle(100, 120)).toEqual(-34);
-  });
+    it('[100, 120] should return -34', () => {
+      expect(rotateAngle(100, 120)).toEqual(-34);
+    });
 
-  it('should return -48', () => {
-    expect(rotateAngle(100, 150)).toEqual(-48);
-  });
+    it('[100, 150] should return -48', () => {
+      expect(rotateAngle(100, 150)).toEqual(-48);
+    });
 
-  it('should return -60', () => {
-    expect(rotateAngle(100, 200)).toEqual(-60);
-  });
+    it('[100, 200] should return -60', () => {
+      expect(rotateAngle(100, 200)).toEqual(-60);
+    });
 
-  it('should return -71', () => {
-    expect(rotateAngle(100, 300)).toEqual(-71);
-  });
+    it('[100, 300] should return -71', () => {
+      expect(rotateAngle(100, 300)).toEqual(-71);
+    });
 
-  it('should return -76', () => {
-    expect(rotateAngle(100, 400)).toEqual(-76);
-  });
+    it('[100, 400] should return -76', () => {
+      expect(rotateAngle(100, 400)).toEqual(-76);
+    });
 
-  it('should return -78', () => {
-    expect(rotateAngle(100, 500)).toEqual(-78);
-  });
+    it('[100, 500] should return -78', () => {
+      expect(rotateAngle(100, 500)).toEqual(-78);
+    });
 
-  it('should return -90', () => {
-    expect(rotateAngle(100, 12000)).toEqual(-90);
+    it('[100, 12000] should return -90', () => {
+      expect(rotateAngle(100, 12000)).toEqual(-90);
+    });
   });
 });
